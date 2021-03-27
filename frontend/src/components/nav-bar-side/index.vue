bf6256515419272751952b3cae03144fadaa5aa8
RoseSavona <905899183@qq.com>
2021-03-27 20:29:43 +0800

    [Added] 新增发送消息页面

--- frontend/src/components/nav-bar-side/index.vue
+++ frontend/src/components/nav-bar-side/index.vue
@@ -1,24 +1,10 @@
 <template>
   <div class="side">
-    <p>XXXXX后台</p>
     <el-menu default-active="2" class="el-menu-vertical" @open="handleOpen" @close="handleClose">
-
-      <!-- <el-submenu index="1">     -->
-        <!-- <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航</span>
-        </template> -->
       <el-menu-item index="1-1">用户人数</el-menu-item>
       <el-menu-item index="1-2">用户列表</el-menu-item>
       <el-menu-item index="1-2">论坛人数</el-menu-item>
 
-      <el-submenu index="" class="lan">
-        <template slot="title">{{ $t('index.language') }}</template>
-        <el-menu-item index="" @click="changeLanguage('zh')">中文</el-menu-item>
-        <el-menu-item index="" @click="changeLanguage('en')">English</el-menu-item>
-      </el-submenu>
-
-      <!-- </el-submenu> -->
   
     </el-menu>
   </div>
---
+++ frontend/src/views/secretary/components/notice.vue
@@ -0,0 +1,42 @@
+<template>
+    <div style="position: fixed;left:5%">
+        <h2>发送消息</h2>
+        <el-input
+            type="text"
+            placeholder="请输入标题"
+            v-model="title"
+            maxlength="30"
+            show-word-limit
+            style="width:70%;margin-bottom:30px"
+            >
+        </el-input>
+        <el-input
+            type="textarea"
+            :autosize="{ minRows: 5}"
+            placeholder="请输入内容"
+            v-model="content"
+            style="margin-bottom:30px">
+        </el-input>
+        <el-button type="primary" plain @click="sendMessage()">发送</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index",
+    data() {
+    return {
+      title: '',
+      content: ''
+    }
+  },
+    methods:{
+        sendMessage(){
+            console.log(this.title+'\n'+this.content);
+        }
+    }
+}
+</script>
+
+<style>
+</style>
