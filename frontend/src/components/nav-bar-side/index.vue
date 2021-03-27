b7bce38ebab0143ff363d87e7dbd2e6e14c71b10
6586744 <67788880+6586744@users.noreply.github.com>
2021-03-27 20:34:08 +0800

    [Update]侧边栏跳转

--- frontend/src/components/nav-bar-side/index.vue
+++ frontend/src/components/nav-bar-side/index.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="side">
     <p>XXXXX后台</p>
-    <el-menu default-active="2" class="el-menu-vertical" @open="handleOpen" @close="handleClose">
-
-      <!-- <el-submenu index="1">     -->
-        <!-- <template slot="title">
-          <i class="el-icon-location"></i>
-          <span>导航</span>
-        </template> -->
-      <el-menu-item index="1-1">用户人数</el-menu-item>
+    <el-menu :default-active="menuActiveIndex" class="el-menu-vertical" :router="true">
+      <el-menu-item v-for="(menuItem,index) in menuList" :index="menuItem.path" :key="menuItem.title" v-if="menuItem.permission == permission">
+        <i :class="menuItem.icon"/>
+        {{ menuItem.title }}
+      </el-menu-item>
+      <!-- <el-menu-item index="1-1">用户人数</el-menu-item>
       <el-menu-item index="1-2">用户列表</el-menu-item>
-      <el-menu-item index="1-2">论坛人数</el-menu-item>
+      <el-menu-item index="1-2">论坛人数</el-menu-item> -->
 
       <el-submenu index="" class="lan">
         <template slot="title">{{ $t('index.language') }}</template>
@@ -30,22 +28,54 @@ export default {
     name: "index",
     
     data(){
-        return{
-            msg: "Welcome to Your Vue.js App"
-        }
+      return {
+        menuActiveIndex: '',
+        menuList: []
+      }
+    },
+    created() {
+      this.reloadMenuData()
+      this.menuActiveIndex  = this.$route.path
+    },
+    computed: {
+      //权限
+      permission() {
+        return 2
+      }
     },
    
     methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      },
       changeLanguage(lan) {
       this.$i18n.locale = lan
       this.reloadMenuData()
-    },
+      },
+      reloadMenuData() {
+      this.menuList = [
+        {
+          //标题
+          title: this.$t('index.personalCenter'),
+          //图表
+          icon: 'el-icon-user-solid',
+          //url路径
+          path: '/chairman',
+          //权限 2为主席， 3为分论坛主席， 4为秘书
+          permission: '2'
+        },
+        {
+          title: this.$t('index.messageCenter'),
+          icon: 'el-icon-message-solid',
+          path: '/detail',
+          permission: '2'
+        },
+        {
+          title: this.$t('index.logout'),
+          icon: 'el-icon-switch-button',
+          path: '/secretary',
+          permission: '2'
+        },
+        
+      ]
+    }
     }
 
 }
