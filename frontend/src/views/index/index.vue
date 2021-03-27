8e6bacc719ead0345d3397c8ef7e56e26eb06616
GaryChan029 <garychan929@ymail.com>
2021-03-27 13:23:37 +0800

    [Updated] 封装顶部导航栏

--- frontend/src/views/index/index.vue
+++ frontend/src/views/index/index.vue
@@ -1,56 +1,19 @@
 <template>
   <div>
-    <el-menu :default-active="menuActiveIndex" class="el-menu-demo" mode="horizontal">
-      <el-menu-item v-for="(menuItem,index) in menuList" :index="(index + 1).toString()" :key="menuItem.title">
-        <i :class="menuItem.icon"/>
-        {{ menuItem.title }}
-      </el-menu-item>
-      <el-submenu index="5">
-        <template slot="title">{{ $t('index.language') }}</template>
-        <el-menu-item index="2-1" @click="changeLanguage('zh')">中文</el-menu-item>
-        <el-menu-item index="2-2" @click="changeLanguage('en')">English</el-menu-item>
-      </el-submenu>
-    </el-menu>
+    <navBarTop />
+    <meetingList />
   </div>
 </template>
 
 <script>
+import navBarTop from '@/components/nav-bar-top'
+import meetingList from './components/meeting-list'
+
 export default {
   name: "index",
-  data() {
-    return {
-      menuActiveIndex: '1',
-      menuList: []
-    }
-  },
-  created() {
-    this.reloadMenuData()
-  },
-  methods: {
-    changeLanguage(lan) {
-      this.$i18n.locale = lan
-      this.reloadMenuData()
-    },
-    reloadMenuData() {
-      this.menuList = [
-        {
-          title: this.$t('index.home'),
-          icon: 'el-icon-s-home'
-        },
-        {
-          title: this.$t('index.personalCenter'),
-          icon: 'el-icon-user-solid'
-        },
-        {
-          title: this.$t('index.messageCenter'),
-          icon: 'el-icon-message-solid'
-        },
-        {
-          title: this.$t('index.logout'),
-          icon: 'el-icon-switch-button'
-        }
-      ]
-    }
+  components: {
+    navBarTop,
+    meetingList
   }
 }
 </script>
