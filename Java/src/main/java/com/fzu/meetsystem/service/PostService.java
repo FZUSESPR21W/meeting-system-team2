ab4f2a0984fddfca6ae25c609f3627b2bf2cd7e7
qingkong <1658851193@qq.com>
2021-03-27 19:59:59 +0800

    [Updated] PostService更新

--- Java/src/main/java/com/fzu/meetsystem/service/PostService.java
+++ Java/src/main/java/com/fzu/meetsystem/service/PostService.java
@@ -1,18 +1,16 @@
 package com.fzu.meetsystem.service;
 
 import com.fzu.meetsystem.pojo.Post;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface PostService {
     //通过用户名、会议id获取对应的文章，如果为null，不用该条件
     //page代表当前页面数，limit代表单页数量，null不限制
-    public List<Post> getPostList(String username,Integer meetId,Integer page,Integer limit);
+    public List<Post> getPostList(Integer meetId,Integer page,Integer limit);
     //发送消息，并通知对应用户
-    public boolean sendPost(String username,String context,Integer meetId);
-    //获取未读文章，并更新最后一次阅读时间
-    public List<Post> getUnreadPost(String username);
-    //获取未读文章数量
-    public Integer getUnreadNumber(String username);
+    public boolean sendPost(String username,Post post);
 
 }
