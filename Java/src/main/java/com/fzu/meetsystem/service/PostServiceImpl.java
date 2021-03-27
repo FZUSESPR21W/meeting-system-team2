fd919372b480d4720c925a0f9a2282790987d887
qingkong <1658851193@qq.com>
2021-03-27 20:05:59 +0800

    [Added] PostService接口实现

---
+++ Java/src/main/java/com/fzu/meetsystem/service/PostServiceImpl.java
@@ -0,0 +1,43 @@
+package com.fzu.meetsystem.service;
+
+import com.fzu.meetsystem.mapper.MeetingDao;
+import com.fzu.meetsystem.mapper.PostDao;
+import com.fzu.meetsystem.mapper.UserDao;
+import com.fzu.meetsystem.pojo.Meeting;
+import com.fzu.meetsystem.pojo.Post;
+import com.fzu.meetsystem.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostServiceImpl implements PostService{
+    @Autowired
+    private PostDao postDao;
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private MeetingDao meetingDao;
+    @Override
+    public List<Post> getPostList(Integer meetId, Integer page, Integer limit) {
+        List<Post> postList = postDao.selectAllPostsByMeetId(meetId, page, limit);
+        return postList;
+    }
+
+    @Override
+    public boolean sendPost(String username, Post post) {
+        User user = userDao.selectUserByUsername(username);
+        postDao.insertPost(post);
+        List<Meeting> meetingList = meetingDao.selectAllMeetsByUsername(username);
+        postDao.pushnew(meetingList.get(0));
+        return true;
+    }
+
+
+
+
+}
