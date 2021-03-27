430016a1b8c7d4ba0149bad6a2e8be770c50f6c2
6586744 <67788880+6586744@users.noreply.github.com>
2021-03-27 19:23:35 +0800

    [Update]主席后台

--- frontend/src/views/chairman/components/echarts.vue
+++ frontend/src/views/chairman/components/echarts.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+    <div id="myChart" :style="{width: '1100px', height: '500px'}"></div>
     
 </template>
  
@@ -13,24 +13,95 @@ export default {
         this.drawLine();
     },
     methods: {
-      drawLine(){
+      drawLine(){  
         // 基于准备好的dom，初始化echarts实例
         let myChart = this.$echarts.init(document.getElementById('myChart'))
         // 绘制图表
-        myChart.setOption({
-            title: { text: '在Vue中使用echarts' },
-            tooltip: {},
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+        myChart.setOption(
+        {
+            title: {
+            text: '关注用户时间分布图',
+            // subtext: '纯属虚构'
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+            type: 'cross'
+            }
+        },
+        toolbox: {
+            show: true,
+            feature: {
+            saveAsImage: {}
+            }
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: ['00:00', '01:30', '03:00', '04:30', '06:00', '07:30', '09:00', '10:30', '12:00', '13:30', '15:00', '16:30', '18:00', '19:30', '21:00', '22:30', '24:00']
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+            formatter: '{value} '
             },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
+            axisPointer: {
+                snap: true
+            }
+        },
+        visualMap: {
+            show: false,
+            dimension: 0,
+            pieces: [{
+                lte: 6,
+                color: 'green'
+            }, 
+            {
+                gt: 6,
+                lte: 8,
+                color: 'red'
+            }, 
+            {
+                gt: 8,
+                lte: 14,
+                color: 'green'
+            }, 
+            {
+                gt: 14,
+                lte: 17,
+                color: 'red'
+            }, 
+            {
+                gt: 17,
+                color: 'green'
             }]
+        },
+        series: [
+            {
+                name: '关注人数',
+                type: 'line',
+                smooth: true,
+                data: [300, 280, 250, 260, 270, 300, 550, 500, 400, 390, 380, 390, 400, 500, 600, 750, 800],
+                // markArea: {
+                //     itemStyle: {
+                //         color: 'rgba(255, 173, 177, 0.4)'
+                //     },
+                //     data: [ [{
+                //         name: '早高峰',
+                //         xAxis: '07:30'
+                //     }, {
+                //         xAxis: '10:00'
+                //     }], [{
+                //         name: '晚高峰',
+                //         xAxis: '17:30'
+                //     }, {
+                //         xAxis: '21:15'
+                //     }] ]
+                // }
+            }
+        ]
         });
-      }
+       }
     }
 
 }
